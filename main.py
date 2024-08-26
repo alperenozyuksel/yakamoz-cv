03264970fe63c554e5035231bd67298a98c1e413
mete <ahmkaradeniz@stu.okan.edu.tr>
2024-08-27 01:52:51 +0300

    kare eklendi

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
 import torch
 import cv2
-from cv2 import resize
+import numpy as np
 
-
+# YOLOv5 modelini yükle
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='model/deneme.onnx')
 
-
+# Video kaynağını aç
 cap = cv2.VideoCapture('video/640640.mp4')
 
-class dogfight():
+class Dogfight:
 
     def __init__(self):
-
         self.yolo()
 
+    def draw_center_square(self, frame, square_size=50, color=(0, 255, 0), thickness=2):
+        # frame'i kopyala, böylece üzerine yazılabilir
+        frame_copy = np.copy(frame)
+
+        # Görüntünün boyutlarını al
+        height, width, _ = frame_copy.shape
+
+        # Karenin sol üst ve sağ alt köşe koordinatlarını hesapla
+        top_left = (width // 2 - square_size // 2, height // 2 - square_size // 2)
+        bottom_right = (width // 2 + square_size // 2, height // 2 + square_size // 2)
+
+        # Kareyi çiz
+        cv2.rectangle(frame_copy, top_left, bottom_right, color, thickness)
+
+        return frame_copy
 
     def yolo(self):
         while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
+            self.ret, self.frame = cap.read()
+            if not self.ret:
                 break
 
-            # Perform inference on the frame
-            results = model(frame)
+            # YOLOv5 modeli ile tahmin yap
+            self.results = model(self.frame)
 
-            # Render the results on the frame
-            frame = results.render()[0]
+            # Sonuçları çerçeveye çiz
+            self.frame = self.results.render()[0]
 
-            # Display the frame
-            cv2.imshow('YOLOv5 Video', frame)
+            # Ekranın ortasına kare çiz
+            self.frame = self.draw_center_square(self.frame, square_size=300, color=(255, 0, 0), thickness=3)
 
-            # Exit on 'q' key press
+            # Çerçeveyi göster
+            cv2.imshow('YOLOv5 Video', self.frame)
+
+            # 'q' tuşuna basıldığında çık
             if cv2.waitKey(30) & 0xFF == ord('q'):
                 break
-
         cap.release()
         cv2.destroyAllWindows()
 
-dogfight()
+# Dogfight sınıfını başlat
+Dogfight()
